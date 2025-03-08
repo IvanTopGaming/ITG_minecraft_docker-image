f0441ff7687c4512a710dc9851c4757e70cf4218
IvanTopGaming <zjarc0@mail.ru>
2025-03-08 20:03:03 +0300

    Added hash verification

--- download_utils/get/get_paper_core.py
+++ download_utils/get/get_paper_core.py
@@ -1,10 +1,44 @@
 import httpx
 import re
+import hashlib
+import sys
+
 
 HEADERS = {
 	"Accept": "application/json",
 	"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
 }
+
+
+def get_file_hash(algorithm):
+	if algorithm == 'sha256':
+		hash = hashlib.sha256()
+	elif algorithm == 'md5':
+		hash = hashlib.md5()
+	elif algorithm == 'sha1':
+		hash = hashlib.sha1()
+	else:
+		print("Unknown hash algorithm")
+		sys.exit(-1)
+
+	with open('server.jar',"rb") as f:
+		for byte_block in iter(lambda: f.read(4096),b""):
+			hash.update(byte_block)
+		
+	return hash.hexdigest()
+
+
+async def compare_hash(version, file_hash, algorithm):
+	async with httpx.AsyncClient() as client:
+		url = f'https://api.papermc.io/v2/projects/paper/versions/{version}/builds/'
+		
+		response = await client.get(url=url, headers=HEADERS)
+		
+		if response.status_code == 200:
+			true_hash = response.json()['builds'][-1]['downloads']['application'][algorithm]
+
+		if true_hash == file_hash:
+			return True
 
 
 async def get_latest_build_name(version: str):
@@ -44,6 +78,15 @@ async def download_build(version: str, build: str):
 				with open("server.jar", "wb") as file:
 					async for chunk in response.aiter_bytes():
 						file.write(chunk)
+	
+	algorithm = "sha256"
+	file_hash = get_file_hash(algorithm)
+
+	if await compare_hash(version, file_hash, algorithm):
+		print("Hashes match")
+	else:
+		print("Hashes do not match")
+		sys.exit(-1)
 
 
 async def get_paper_core(version: str):
--- download_utils/get/get_vanilla_core.py
+++ download_utils/get/get_vanilla_core.py
@@ -1,9 +1,36 @@
 import httpx
+import hashlib
+import sys
+
 
 HEADERS = {
 	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 	"User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
 }
+
+
+def get_file_hash(algorithm):
+	if algorithm == 'sha256':
+		hash = hashlib.sha256()
+	elif algorithm == 'md5':
+		hash = hashlib.md5()
+	elif algorithm == 'sha1':
+		hash = hashlib.sha1()
+	else:
+		print("Unknown hash algorithm")
+		sys.exit(-1)
+
+	with open('server.jar',"rb") as f:
+		for byte_block in iter(lambda: f.read(4096),b""):
+			hash.update(byte_block)
+		
+	return hash.hexdigest()
+
+
+async def compare_hash(true_hash, file_hash):
+	async with httpx.AsyncClient() as client:
+		if true_hash == file_hash:
+			return True
 
 
 async def get_versions():
@@ -33,7 +60,7 @@ async def get_core_url(version_url: str):
 		response = await client.get(url=version_url, headers=HEADERS)
 
 		if response.status_code == 200:
-			return response.json()['downloads']['server']['url']
+			return response.json()['downloads']['server']
 		
 		print("Failed to get core url")
 		return None
@@ -41,11 +68,20 @@ async def get_core_url(version_url: str):
 
 async def download_build(core_url):
 	async with httpx.AsyncClient() as client:
-		async with client.stream("GET", core_url, headers=HEADERS) as response:
+		async with client.stream("GET", core_url['url'], headers=HEADERS) as response:
 			if response.status_code == 200:
 				with open("server.jar", "wb") as file:
 					async for chunk in response.aiter_bytes():
 						file.write(chunk)
+
+	algorithm = "sha1"
+	file_hash = get_file_hash(algorithm)
+
+	if await compare_hash(core_url[algorithm], file_hash):
+		print("Hashes match")
+	else:
+		print("Hashes do not match")
+		sys.exit(-1)
 
 
 async def get_vanilla_core(version: str):
