38d7a4691a303e11c2db0335676ee9af801590cc
IvanTopGaming <85171599+IvanTopGaming@users.noreply.github.com>
2025-04-20 03:26:07 +0300

    Port to zulu-24

--- scripts/build_and_push_all.bat
+++ scripts/build_and_push_all.bat
@@ -1,4 +1,4 @@
-docker build -t docker.io/ivantopgaming/minecraft_server:zulu-23 -f Dockerfile.zulu-23 .
+docker build -t docker.io/ivantopgaming/minecraft_server:zulu-24 -f Dockerfile.zulu-24 .
 docker build -t docker.io/ivantopgaming/minecraft_server:zulu-21 -f Dockerfile.zulu-21 .
 docker build -t docker.io/ivantopgaming/minecraft_server:zulu-17 -f Dockerfile.zulu-17 .
 docker build -t docker.io/ivantopgaming/minecraft_server:zulu-11 -f Dockerfile.zulu-11 .
@@ -8,7 +8,7 @@ docker build -t docker.io/ivantopgaming/minecraft_server:graalvm-21 -f Dockerfil
 docker build -t docker.io/ivantopgaming/minecraft_server:latest .
 
 
-docker push docker.io/ivantopgaming/minecraft_server:zulu-23
+docker push docker.io/ivantopgaming/minecraft_server:zulu-24
 docker push docker.io/ivantopgaming/minecraft_server:zulu-21
 docker push docker.io/ivantopgaming/minecraft_server:zulu-17
 docker push docker.io/ivantopgaming/minecraft_server:zulu-11
