f174f281125e7e1c9b0aabde82ad629cc4f5edcc
IvanTopGaming <85171599+IvanTopGaming@users.noreply.github.com>
2025-11-14 14:53:44 +0300

    Added POST_JVM_OPTS environment variable

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@
 /bin/api &
 
 if [ "$ENABLE_LOG4J_PATCH" = "true" ]; then
-    exec java -javaagent:/bin/Log4jPatcher.jar ${JVM_OPTS} -jar ${CORE_JAR} nogui
+    exec java -javaagent:/bin/Log4jPatcher.jar ${JVM_OPTS} -jar ${CORE_JAR} ${POST_JVM_OPTS} nogui
 else
-    exec java ${JVM_OPTS} -jar ${CORE_JAR} nogui
+    exec java ${JVM_OPTS} -jar ${CORE_JAR} ${POST_JVM_OPTS} nogui
 fi
